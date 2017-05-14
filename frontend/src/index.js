196e6612ff5805053f007721c40da9984075c190
Ajay Roopakalu <ajay.roopakalu@gmail.com>
2017-05-14 11:55:57 -0400

    frontend: Downgrade to react-router v3 for now.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Router, Route, browserHistory } from 'react-router';
 import App from './App';
 import Login from './components/Login';
 
 ReactDOM.render((
-    <Router>
+    <Router history={browserHistory}>
       <div className="App">
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/' component={App} />
+        <Route path='/login' component={Login} />
+        <Route path='/' component={App} />
       </div>
     </Router>),
     document.getElementById('root')
