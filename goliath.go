9940cf60722904f87200b49bf5d19d12c2db7455
Ajay Roopakalu <ajay.roopakalu@gmail.com>
2017-07-08 14:46:50 -0400

    core: Add new HTTP server for exposing Prometheus metrics.

--- goliath.go
+++ goliath.go
@@ -17,6 +17,7 @@ import (
 	"time"
 	"encoding/json"
 	"strconv"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 const VERSION = "0.01"
@@ -25,12 +26,13 @@ var (
 	dbPath   = flag.String("dbPath", "", "The address of the database.")
 	opmlPath = flag.String("opmlPath", "", "Path of OPML file to import.")
 	port = flag.Int("port", 9999, "Port of HTTP server.")
+	metricsPort = flag.Int("metricsPort", 9998, "Port to expose Prometheus metrics.")
 	publicFolder = flag.String("publicFolder", "public", "Location of static content to serve.")
 )
 
-// Linker-initialized compile-time variables.
-var buildTimestamp string
-var buildHash string
+// Linker-overridden variables.
+var buildTimestamp = "<unknown>"
+var buildHash = "<unknown>"
 
 func main() {
 	flag.Parse()
@@ -41,11 +43,11 @@ func main() {
 	log.Infof("Goliath %s.", VERSION)
 	t, err := strconv.ParseInt(buildTimestamp, 10, 64)
 	if err != nil {
-		log.Warningf("Invalid build timestamp %s: %s", buildTimestamp, err)
+		log.V(2).Infof("Invalid build timestamp %s: %s", buildTimestamp, err)
 	} else {
 		buildTimestamp = time.Unix(t, 0).String()
-		log.Infof("Built at: %s", buildTimestamp)
 	}
+	log.Infof("Build time: %s", buildTimestamp)
 	log.Infof("Build hash: %s", buildHash)
 
 	if *dbPath == "" {
@@ -77,6 +79,12 @@ func main() {
 	go fetch.Start(ctx, d)
 	go storage.StartGc(ctx, d)
 
+	go func() {
+		if err = ServeMetrics(ctx); err != nil {
+			log.Infof("%s", err)
+		}
+	}()
+
 	if err = Serve(ctx, d); err != nil {
 		log.Infof("%s", err)
 	}
@@ -93,12 +101,39 @@ func installSignalHandler(cancel context.CancelFunc) {
 	}()
 }
 
+func ServeMetrics(ctx context.Context) error {
+	mux := http.NewServeMux()
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%d", *metricsPort),
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		MaxHeaderBytes: 1 << 10,
+		Handler: mux,
+	}
+
+	go func(srv *http.Server) {
+		select {
+		case <- ctx.Done():
+			log.Infof("Shutting down metrics server.")
+			if err := srv.Shutdown(nil); err != nil {
+				log.Infof("Failed to cleanly shutdown metrics server: %s", err)
+			}
+		}
+	}(srv)
+
+	mux.Handle("/metrics", promhttp.Handler())
+	log.Infof("Starting metrics server on %s", srv.Addr)
+	return srv.ListenAndServe()
+}
+
 func Serve(ctx context.Context, d *storage.Database) error {
+	mux := http.NewServeMux()
 	srv := &http.Server{
 		Addr: fmt.Sprintf(":%d", *port),
 		ReadTimeout: 10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		MaxHeaderBytes: 1 << 10,
+		Handler: mux,
 	}
 
 	go func(srv *http.Server) {
@@ -111,13 +146,13 @@ func Serve(ctx context.Context, d *storage.Database) error {
 		}
 	}(srv)
 
-	http.HandleFunc("/auth", auth.HandleLogin(d));
-	http.HandleFunc("/logout", auth.HandleLogout);
-	http.HandleFunc("/fever/", HandleFever(d))
-	http.HandleFunc("/version", HandleVersion)
-	http.Handle("/static/", http.FileServer(http.Dir(*publicFolder)))
-	http.Handle("/", auth.WithAuth(http.FileServer(http.Dir(*publicFolder)), d, *publicFolder))
-	log.Infof("Starting HTTP server on port %d", *port)
+	mux.HandleFunc("/auth", auth.HandleLogin(d));
+	mux.HandleFunc("/logout", auth.HandleLogout);
+	mux.HandleFunc("/fever/", HandleFever(d))
+	mux.HandleFunc("/version", HandleVersion)
+	mux.Handle("/static/", http.FileServer(http.Dir(*publicFolder)))
+	mux.Handle("/", auth.WithAuth(http.FileServer(http.Dir(*publicFolder)), d, *publicFolder))
+	log.Infof("Starting HTTP server on %s", srv.Addr)
 	return srv.ListenAndServe()
 }
 
