74094095d30893a3b2d633675fb9a9b0e3af1d23
Ajay Roopakalu <ajay@roopakalu.com>
2022-06-27 15:08:54 -0400

    frontend: Refactor some inline CSS to classes and use elevation for read state.

--- frontend/src/components/SplitViewArticleListEntry.tsx
+++ frontend/src/components/SplitViewArticleListEntry.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {ArticleImagePreview, ArticleListEntry} from "../utils/types";
-import {Grid, Typography} from "@mui/material";
+import {Grid, Paper, Typography} from "@mui/material";
 
 export interface SplitViewArticleListEntryProps {
   article: ArticleListEntry,
@@ -26,7 +26,7 @@ export default class SplitViewArticleListEntry
       return;
     }
 
-    const minPixelSize = 100;
+    const minPixelSize = 100, imgFetchLimit = 5;
     const p: Promise<any>[] = [];
     const [article] = this.props.article;
     const images = new DOMParser()
@@ -36,7 +36,7 @@ export default class SplitViewArticleListEntry
       return;
     }
 
-    let limit = Math.min(5, images.length)
+    let limit = Math.min(imgFetchLimit, images.length)
     for (let j = 0; j < limit; j++) {
       const img = new Image();
       img.src = images[j].src;
@@ -48,6 +48,7 @@ export default class SplitViewArticleListEntry
           }
           reject();
         }).catch(() => {
+          /* Ignore errors */
         });
       }));
     }
@@ -89,45 +90,44 @@ export default class SplitViewArticleListEntry
       faviconImg = <img src={`data:${favicon}`} height={16} width={16} alt=''/>
     }
 
-    let css: Record<string, string> = {
-      borderBottom: "1px solid gray",
-      padding: "10px",
-    };
-
+    let elevation = 3, readClass = "";
     if (this.props.selected) {
-      css['background'] = "rgb(20, 20, 20)";
+      elevation = 10;
     } else if (article.is_read === 1) {
-      css['background'] = "rgb(5, 5, 5)"
+      elevation = 0;
+      readClass = "GoliathArticleListRead"
     }
 
     return (
-      <Grid container direction="column" style={css}>
-        <Grid zeroMinWidth item className="GoliathSplitViewArticleListGrid">
-          <Typography noWrap className="GoliathArticleListTitle">
-            {extractContent(article.title)}
-          </Typography>
-        </Grid>
-        <Grid zeroMinWidth container item xs>
-          <Grid item sx={{paddingRight: "10px"}} xs="auto">
-            {faviconImg}
-          </Grid>
-          <Grid item zeroMinWidth xs>
-            <Typography noWrap className="GoliathArticleFeedTitle">
-              {extractContent(title)}
+      <Paper elevation={elevation} className={readClass}>
+        <Grid container direction="column" className="GoliathArticleListEntry">
+          <Grid zeroMinWidth item className="GoliathArticleListTitle">
+            <Typography noWrap className="GoliathArticleListTitle">
+              {extractContent(article.title)}
             </Typography>
           </Grid>
-        </Grid>
-        <Grid container item wrap="nowrap">
-          <Grid item xs='auto'>
-            {this.state.preview}
+          <Grid zeroMinWidth container item xs>
+            <Grid item sx={{paddingRight: "10px"}} xs="auto">
+              {faviconImg}
+            </Grid>
+            <Grid item zeroMinWidth xs>
+              <Typography noWrap className="GoliathArticleFeedTitle">
+                {extractContent(title)}
+              </Typography>
+            </Grid>
           </Grid>
-          <Grid item zeroMinWidth xs style={{height: '100px'}}>
-            <Typography className="GoliathArticleContentPreview">
-              {extractContent(article.html)}
-            </Typography>
+          <Grid container item wrap="nowrap">
+            <Grid item xs='auto'>
+              {this.state.preview}
+            </Grid>
+            <Grid item zeroMinWidth xs style={{height: '100px'}}>
+              <Typography className="GoliathArticleContentPreview">
+                {extractContent(article.html)}
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      </Paper>
     );
   }
 }
