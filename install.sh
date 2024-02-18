a85bd0c03640420cf1e7a8f91a8365ac8eb67e0b
Ajay Roopakalu <ajay@roopakalu.com>
2024-02-17 19:44:37 -0500

    core: Update `install.sh` script to build/deploy Goliath via Docker compose.

--- install.sh
+++ install.sh
@@ -1,34 +1,100 @@
-# A script to build and package Goliath.
-
 #!/usr/bin/env bash
 
-set -euxo pipefail
+# A script to build and package Goliath.
 
-echo "Building Goliath core."
+set -euo pipefail
 
-echo "Compiling protobufs."
-protoc -I admin/ admin/admin.proto --go_out=plugins=grpc:admin
+CRDB_SRC_DIR="cockroach-data"
+CRDB_VOL_NAME="crdb_volume"
+CRDB_DEST_DIR="."
+FORCE_VOL=false
+DOCKER_COMPOSE_ARGS=()
+BUILD_TIMESTAMP=$(date +%s)
+BUILD_HASH=$(git rev-parse HEAD)
 
-buildTimestamp=`date +%s`
-buildHash=`git rev-parse HEAD`
-ldFlags="-X main.buildTimestamp=$buildTimestamp -X main.buildHash=$buildHash"
-echo "Compling Goliath core."
-go build -x -v -mod=mod -ldflags "$ldFlags" .
+# Export variables to make them visible during the build process
+export BUILD_TIMESTAMP
+export BUILD_HASH
 
-echo "Compiling Goliath frontend."
-cd frontend/
-npm run build
-cd ..
+function usage() {
+ echo "Usage: $0 [OPTIONS]"
+ echo "Options:"
+ echo " -h, --help          Display this help message."
+ echo " --crdb_dir DIR  CockroachDB data directory to populate volume. Ignored if volume exists unless --force_volume is set."
+ echo " --force_volume  Force creation of volume even if already exists (destroys existing volume)"
+ echo " --force_build   Force a rebuild before starting containers."
+}
 
-echo "Putting outputs in 'out/' folder."
-if [[ -d ./out/ ]]; then
-    rm -rf ./out/
+function handle_options() {
+  while [ $# -gt 0 ]; do
+    case $1 in
+      -h | --help)
+        usage
+        exit 0
+        ;;
+      -d | --crdb_dir)
+        CRDB_SRC_DIR="$2"
+        if [[ -z "${CRDB_SRC_DIR}" ]]; then
+          echo "Value of crdb dir flag must be non-empty. Exiting."
+          exit 1
+        fi
+        if [[ ! -d "${CRDB_SRC_DIR}" ]]; then
+          echo "Directory specified by crdb dir flag does not exist: ${CRDB_SRC_DIR}. Exiting."
+          exit 1
+        fi
+
+        shift
+        ;;
+      --force_build)
+        DOCKER_COMPOSE_ARGS+=("--build")
+        ;;
+      --force_volume)
+        FORCE_VOL=true
+        ;;
+      *)
+        echo "Invalid option: $1" >&2
+        usage
+        exit 1
+        ;;
+    esac
+    shift
+  done
+}
+
+# Inspired by https://stackoverflow.com/a/68511611.
+function copy_to_volume() {
+  SRC_PATH=$1
+  DEST_VOLUME_NAME=$2
+  DEST_PATH="${3:-}"
+
+  # Create minimal, empty container.
+  echo -e 'FROM scratch\nLABEL empty=""' | docker build -t empty -
+
+  CONTAINER_ID=$(docker container create -v "${DEST_VOLUME_NAME}:/cockroach-data" empty cmd)
+  docker cp "${SRC_PATH}/" "${CONTAINER_ID}:${DEST_PATH}"
+  docker rm "${CONTAINER_ID}" --volumes
+}
+
+# Flag parsing.
+handle_options "$@"
+
+if [[ $FORCE_VOL = true ]]; then
+  echo "Destroying existing CockroachDB data volume: ${CRDB_VOL_NAME} ..."
+
+  # Make sure all volumes referenced in the Compose file are downed.
+  docker compose down --volumes
+  # Then make sure any stopped containers in the Compose file are also removed.
+  docker compose rm --volumes
+  docker volume rm -f "${CRDB_VOL_NAME}" > /dev/null
 fi
-mkdir -p out/public
 
-cp ./goliath ./out/
-cp -R frontend/build/* ./out/public/
+if [[ -z $(docker volume ls -f name="${CRDB_VOL_NAME}" -q) ]]; then
+  echo "Creating CockroachDB data volume..."
 
-cd ./out/
-ln -s ../frontend/node_modules/@postlight/mercury-parser/cli.js mercury-parser
-ln -s ../config.ini .
+  docker volume create --name "${CRDB_VOL_NAME}" > /dev/null
+  copy_to_volume "${CRDB_SRC_DIR}" "${CRDB_VOL_NAME}" "${CRDB_DEST_DIR}"
+fi
+
+echo "Starting container..."
+
+docker compose up "${DOCKER_COMPOSE_ARGS[@]}"
