3a142cd20f15b80496084743ab905926443a2167
Ajay Roopakalu <ajay@roopakalu.com>
2025-08-10 15:13:00 -0400

    frontend: Run Oxlint on Loading.test.tsx.

--- frontend/src/components/__tests__/Loading.test.tsx
+++ frontend/src/components/__tests__/Loading.test.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
-import {describe, expect, it} from 'vitest';
-import Loading, {LoadingProps} from '../Loading';
-import {Status} from "../../utils/types";
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import Loading, { LoadingProps } from '../Loading';
+import { Status } from '../../utils/types';
 
-const statusToString = (status: Status): String => {
-  const statusRatio = status /
-    (Status.Folder | Status.Feed | Status.Article | Status.Favicon);
+const statusToString = (status: Status): string => {
+  const statusRatio =
+    status / (Status.Folder | Status.Feed | Status.Article | Status.Favicon);
   return Math.round(100 * statusRatio).toString();
-}
+};
 
 describe('Loading', () => {
   it('renders the correct progress for single Status', () => {
@@ -21,7 +21,10 @@ describe('Loading', () => {
 
     const progressBar = screen.getByTestId('loading-progress');
     expect(progressBar).toBeInTheDocument();
-    expect(progressBar).toHaveAttribute('aria-valuenow', statusToString(status))
+    expect(progressBar).toHaveAttribute(
+      'aria-valuenow',
+      statusToString(status)
+    );
   });
   it('renders the correct progress for mixed Status', () => {
     const status = Status.Folder | Status.Article;
@@ -33,10 +36,13 @@ describe('Loading', () => {
 
     const progressBar = screen.getByTestId('loading-progress');
     expect(progressBar).toBeInTheDocument();
-    expect(progressBar).toHaveAttribute('aria-valuenow', statusToString(status))
+    expect(progressBar).toHaveAttribute(
+      'aria-valuenow',
+      statusToString(status)
+    );
   });
 
   it('renders without crashing', () => {
-    render(<Loading status={0}/>)
+    render(<Loading status={0} />);
   });
 });
