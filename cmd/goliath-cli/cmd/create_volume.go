a2394ec1628a0779464d555a8b0e374c22e79cd7
Ajay Roopakalu <ajay@roopakalu.com>
2025-10-18 17:58:09 -0400

    cmd: Add "create-volume" CLI command.
    This commit also adds the go.mod and go.sum files that include future
    changes.

---
+++ cmd/goliath-cli/cmd/create_volume.go
@@ -0,0 +1,116 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	crdbVolumeName  = "crdb_volume"
+	crdbVolumeMount = "/cockroach-data"
+)
+
+var createVolumeCmd = &cobra.Command{
+	Use:   "create-volume",
+	Short: "Creates the CockroachDB data volume",
+	Run: func(cmd *cobra.Command, args []string) {
+		dir, _ := cmd.Flags().GetString("dir")
+		force, _ := cmd.Flags().GetBool("force")
+
+		if _, err := os.Stat(dir); os.IsNotExist(err) {
+			fmt.Printf("Directory specified by --dir flag does not exist: %s. Exiting.\n", dir)
+			os.Exit(1)
+		}
+
+		volumeExistsCmd := exec.Command("docker", "volume", "ls", "-f", "name="+crdbVolumeName, "-q")
+		existingVolume, err := volumeExistsCmd.Output()
+		if err != nil {
+			fmt.Println("Error checking for docker volume:", err)
+			os.Exit(1)
+		}
+		volumeExists := strings.TrimSpace(string(existingVolume)) != ""
+
+		if force && volumeExists {
+			fmt.Println("Destroying existing CockroachDB data volume:", crdbVolumeName)
+			downCmd := exec.Command("docker", "compose", "down", "--volumes")
+			downCmd.Stdout = os.Stdout
+			downCmd.Stderr = os.Stderr
+			if err := downCmd.Run(); err != nil {
+				fmt.Println("Error running 'docker compose down --volumes':", err)
+				os.Exit(1)
+			}
+
+			rmVolCmd := exec.Command("docker", "volume", "rm", "-f", crdbVolumeName)
+			rmVolCmd.Stdout = os.Stdout
+			rmVolCmd.Stderr = os.Stderr
+			if err := rmVolCmd.Run(); err != nil {
+				fmt.Println("Error removing docker volume:", err)
+				os.Exit(1)
+			}
+			volumeExists = false
+		}
+
+		if !volumeExists {
+			fmt.Println("Creating CockroachDB data volume...")
+			createVolCmd := exec.Command("docker", "volume", "create", "--name", crdbVolumeName)
+			if err := createVolCmd.Run(); err != nil {
+				fmt.Println("Error creating docker volume:", err)
+				os.Exit(1)
+			}
+
+			fmt.Println("Copying data to volume...")
+			copyToVolume(dir)
+		} else {
+			fmt.Println("Volume", crdbVolumeName, "already exists. Use --force to recreate.")
+		}
+
+		fmt.Println("Volume setup complete.")
+	},
+}
+
+func copyToVolume(srcPath string) {
+	dockerBuildCmd := exec.Command("docker", "build", "-t", "empty", "-")
+	dockerBuildCmd.Stdin = strings.NewReader("FROM scratch\nLABEL empty=''''")
+	if err := dockerBuildCmd.Run(); err != nil {
+		fmt.Println("Error creating empty docker image:", err)
+		os.Exit(1)
+	}
+
+	containerCreateCmd := exec.Command("docker", "container", "create", "-v", crdbVolumeName+":"+crdbVolumeMount, "empty", "cmd")
+	containerIDBytes, err := containerCreateCmd.Output()
+	if err != nil {
+		fmt.Println("Error creating container with volume:", err)
+		os.Exit(1)
+	}
+	containerID := strings.TrimSpace(string(containerIDBytes))
+
+	cpSrcPath := srcPath
+	if !strings.HasSuffix(cpSrcPath, "/") {
+		cpSrcPath += "/"
+	}
+	dockerCpCmd := exec.Command("docker", "cp", cpSrcPath, containerID+":"+crdbVolumeMount)
+	dockerCpCmd.Stdout = os.Stdout
+	dockerCpCmd.Stderr = os.Stderr
+	if err := dockerCpCmd.Run(); err != nil {
+		fmt.Println("Error copying data to volume:", err)
+		exec.Command("docker", "rm", containerID, "--volumes").Run()
+		os.Exit(1)
+	}
+
+	dockerRmCmd := exec.Command("docker", "rm", containerID, "--volumes")
+	if err := dockerRmCmd.Run(); err != nil {
+		fmt.Println("Error removing temporary container:", err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	createVolumeCmd.GroupID = "setup"
+	rootCmd.AddCommand(createVolumeCmd)
+	createVolumeCmd.Flags().String("dir", "cockroach-data", "CockroachDB data directory to populate volume from")
+	createVolumeCmd.Flags().Bool("force", false, "Force creation of volume even if it already exists (destroys existing volume)")
+}
---
+++ cmd/goliath-cli/go.mod
@@ -0,0 +1,69 @@
+module github.com/jrupac/goliath/cmd/goliath-cli
+
+go 1.24.0
+
+// Workaround since the module path for the backend is "github.com/jrupac/goliath" while it should be "github.com/jrupac/goliath/backend".
+// TODO: Remove when the module naming of the backend library is corrected.
+replace github.com/jrupac/goliath => ../../backend
+
+toolchain go1.24.9
+
+require (
+	github.com/jrupac/goliath v0.0.0-00010101000000-000000000000
+	github.com/spf13/cobra v1.10.1
+	google.golang.org/grpc v1.76.0
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.10.2 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/arbovm/levenshtein v0.0.0-20160628152529-48b4e1c0c4d0 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/charmbracelet/bubbles v0.21.0 // indirect
+	github.com/charmbracelet/bubbletea v1.3.10 // indirect
+	github.com/charmbracelet/colorprofile v0.2.3-0.20250311203215-f60798e515dc // indirect
+	github.com/charmbracelet/lipgloss v1.1.0 // indirect
+	github.com/charmbracelet/x/ansi v0.10.1 // indirect
+	github.com/charmbracelet/x/cellbuf v0.0.13-0.20250311204145-2c3ea96c31dd // indirect
+	github.com/charmbracelet/x/term v0.2.1 // indirect
+	github.com/dgryski/go-metro v0.0.0-20211217172704-adc40b04c140 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/golang/glog v1.2.5 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/gorilla/css v1.0.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jrupac/rss v1.0.8 // indirect
+	github.com/kljensen/snowball v0.10.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mat/besticon/v3 v3.21.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.27 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/termenv v0.16.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/prometheus/client_golang v1.21.1 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.62.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/seiflotfy/cuckoofilter v0.0.0-20240715131351-a2f2c23f1771 // indirect
+	github.com/spf13/pflag v1.0.9 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/crypto v0.40.0 // indirect
+	golang.org/x/image v0.25.0 // indirect
+	golang.org/x/net v0.42.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/text v0.27.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250804133106-a7a43d27e69b // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
+)
